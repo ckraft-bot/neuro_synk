45a4b9fc5bdab457f5c62bd0f096f5ae70f0209f
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2025-02-18 10:10:40 -0500

    feedback implemented
    smaller kofi logo, less attention on it

--- App/utility.py
+++ App/utility.py
@@ -4,8 +4,8 @@ OLLAMA_URL = "http://157.230.187.207:11434" #"http://localhost:11434/api/generat
 def donate():
     """Streamlit donation options."""
     st.sidebar.markdown(
-        '<a href="https://ko-fi.com/clairekraft" target="_blank">'
-        '<img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" width="200" alt="Buy Me a Coffee">'
-        '</a>',
-        unsafe_allow_html=True
+            '<a href="https://ko-fi.com/clairekraft" target="_blank">'
+            '<img src="https://miro.medium.com/v2/resize:fit:1200/1*HdRAxEVwO_27UL1e6QhUeA.png" width="100" alt="Buy Me a Coffee">'
+            '</a>',
+            unsafe_allow_html=True
     )
