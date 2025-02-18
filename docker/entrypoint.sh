26c96f2bf3e5bcf62def974d67943f6b8219b7fc
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2025-02-17 23:30:02 -0500

    changed dockerfile
    error building image on digital ocean droplet

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,1 +1,17 @@
-chmod +x docker/entrypoint.sh
+#!/bin/bash
+
+# Start Ollama in the background
+ollama serve &
+
+# Give Ollama some time to initialize
+sleep 10
+
+# Pull the Gemma 2B model
+ollama pull gemma2:2b
+
+# Run the status checks
+ollama ps
+ollama list
+
+# Keep the container running
+wait $!
