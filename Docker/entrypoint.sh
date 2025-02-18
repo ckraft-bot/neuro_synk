87e9cf1f13bca336645454459209411b8741a4bc
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2025-02-17 22:49:10 -0500

    files reorg

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-ollama serve &   # Start Ollama in the background
-sleep 5          # Wait for initialization
-ollama ps
-ollama list
-wait             # Keep the container running
+chmod +x docker/entrypoint.sh
