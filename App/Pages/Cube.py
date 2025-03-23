3fd8147d717b187ef956b96e12eb316d6a50784b
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2025-03-23 00:25:11 -0400

    title updated for cube tab
    cube tab  is not showing up

--- App/Pages/Cube.py
+++ App/Pages/Cube.py
@@ -1,8 +1,8 @@
 import streamlit as st  
 import streamlit.components.v1 as components
 
-st.title("Interactive 3D Rubik's Cube")  
-st.write("Solve or play with the Rubik’s Cube below!")  
+st.title("Solve the cube")
+st.write("Interactive 3D Rubik’s Cube")
 
 # Embed an interactive Rubik's Cube from CodePen
 components.iframe("https://codepen.io/oliverbenns/full/LYpQybz", height=600)
